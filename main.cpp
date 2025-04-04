e3e16f8848731e26c6ee32fa9d9066add32daf3c
Hilly Gangolf <gangolfa@carleton.edu>
2025-04-04 09:23:16 -0500

    starting

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include "Empirical/include/emp/math/Random.hpp"
 
-int main() {
+// First you’ll need an Organism class that has an instance variable double behavior and a constructor that takes a double and sets the instance variable.
+class Organism
+{
+public:
+    double behavior;
 
+    // Constructor that takes a double and sets the instance variable
+    Organism(double b) : behavior(b) {}
+};
+
+int main()
+{
+    emp::vector<Organism> population;
+
+    // Then fill your population with 100 Organisms with all the same starting behavior
+    for (int i = 0; i < 100; ++i)
+    {
+        population.push_back(*new Organism(0.5)); // All starting with behavior 0.5
+
+        // Print the behavior of one organism
+        std::cout << "Organism " << i << " behavior: " << population[i].behavior << std::endl;
+    }
 }
